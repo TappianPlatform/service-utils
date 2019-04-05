e92a09c393469341988becf93f64033684f76302
Dustin Reagan <drreagan@gmail.com>
2019-04-05 14:10:39 -0500

    add createCRUDHandlers

--- service-utils.js
+++ service-utils.js
@@ -10,3 +10,76 @@ exports.auth = (fn, allowedRoles) => {
     fn(payload,callbacks, id)
   }
 }
+
+exports.createCRUDHandlers = ({ modelName, sequelizeModel, sequelizeInstance, allowedRoles }) => {
+  return {
+    beforeAll: async done => {
+      try {
+        await sequelizeInstance.authenticate()
+        console.log('Connected to postgres database') // eslint-disable-line no-console
+      } catch (error) {
+        console.error('Unable to connect to the postgres database:', error) // eslint-disable-line no-console
+      }
+      done()
+    },
+
+    [`get ${modelName}`]: exports.auth(async ({ id }, { reply }) => {
+      try {
+        const instance = await sequelizeModel.findById(id)
+        if (instance) {
+          reply(`${modelName} details`, instance)
+        } else {
+          reply(`${modelName} not-found`, instance)
+        }
+      } catch (error) {
+        console.error('Error getting: ', error) // eslint-disable-line no-console
+      }
+    }, allowedRoles),
+
+    [`list ${modelName}`]: exports.auth(async (_, { reply }) => {
+      try {
+        const instances = await sequelizeModel.findAll()
+        reply(`${modelName} list`, instances)
+      } catch (error) {
+        console.error('Error listing: ', error) // eslint-disable-line no-console
+      }
+    }, allowedRoles),
+
+    [`create ${modelName}`]: exports.auth(async (data, { reply }) => {
+      try {
+        const instance = await sequelizeModel.create(data)
+        reply(`${modelName} created`, instance)
+      } catch (error) {
+        console.error('Error creating: ', error) // eslint-disable-line no-console
+      }
+    }, allowedRoles),
+
+    [`update ${modelName}`]: exports.auth(async (data, { reply }) => {
+      try {
+        const instance = await sequelizeModel.findById(data.id)
+        if (instance) {
+          await instance.update(data)
+          reply(`${modelName} updated`, instance)
+        } else {
+          reply(`${modelName} not-found`)
+        }
+      } catch (error) {
+        console.error('Error updating: ', error) // eslint-disable-line no-console
+      }
+    }, allowedRoles),
+
+    [`delete ${modelName}`]: exports.auth(async ({ id }, { reply }) => {
+      try {
+        const instance = await sequelizeModel.findById(id)
+        if (instance) {
+          await instance.destroy()
+          reply(`${modelName} deleted`, instance)
+        } else {
+          reply(`${modelName} not-found`)
+        }
+      } catch (error) {
+        console.error('Error deleting: ', error) // eslint-disable-line no-console
+      }
+    }, allowedRoles),
+  }
+}
